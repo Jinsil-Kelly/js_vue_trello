0c5dcce655b8b80637edb24bb14067e223667852
KellyRose <kellyrose.evergreen@gmail.com>
2019-04-29 18:25:34 +0100

    removed gitHooks:pre-commit(lint-staged) temporarily...

--- src/store/modules/card/getters.js
+++ src/store/modules/card/getters.js
@@ -1,3 +1,3 @@
 export default {
-  card: state => state.card
+  card: state => state || {}
 };
--- tests/unit/fixtures/board.js
+++ tests/unit/fixtures/board.js
@@ -2,16 +2,16 @@ export default {
   id: 2,
   title: "MockTitleForTest",
   bgColor: "rgb(0, 121, 191)",
-  createdAt: "2019-04-28T17:49:13.383Z",
-  updatedAt: "2019-04-28T17:49:13.383Z",
+  createdAt: "2019-04-29T15:34:10.244Z",
+  updatedAt: "2019-04-29T15:34:10.244Z",
   userId: 1,
   lists: [
     {
       id: 1,
       title: "Todo",
       pos: 65535,
-      createdAt: "2019-04-28T17:49:13.427Z",
-      updatedAt: "2019-04-28T17:49:13.427Z",
+      createdAt: "2019-04-29T15:34:10.264Z",
+      updatedAt: "2019-04-29T15:34:10.264Z",
       boardId: 2,
       cards: []
     },
@@ -19,8 +19,8 @@ export default {
       id: 2,
       title: "Doing",
       pos: 131070,
-      createdAt: "2019-04-28T17:49:13.427Z",
-      updatedAt: "2019-04-28T17:49:13.427Z",
+      createdAt: "2019-04-29T15:34:10.264Z",
+      updatedAt: "2019-04-29T15:34:10.264Z",
       boardId: 2,
       cards: []
     },
@@ -28,8 +28,8 @@ export default {
       id: 3,
       title: "Done",
       pos: 262140,
-      createdAt: "2019-04-28T17:49:13.427Z",
-      updatedAt: "2019-04-28T17:49:13.427Z",
+      createdAt: "2019-04-29T15:34:10.264Z",
+      updatedAt: "2019-04-29T15:34:10.264Z",
       boardId: 2,
       cards: []
     }
